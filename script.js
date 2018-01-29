98c726dd5c9b658db370a5c1ecb39ac9910b58a8
Hugo <hugo@macbook-pro-de-hugo.home>
2018-01-29 19:39:40 +0100

    h

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
           $('.navbar').removeClass('scrolled');
         }
       });
+
+});
+
+;(function($) {
+    "use strict";
+    $(document).ready(function() {
+        $('#access').on('touchstart click', '.skip-link', function(event) {
+            $(this).toggleClass('focus');
+            $($(this).attr('href')).toggleClass('target');
+            event.preventDefault();
+        }).find('.skip-link').append('<span>'+$('#menu .active').text()+'</span>');
     });
+})(jQuery);
