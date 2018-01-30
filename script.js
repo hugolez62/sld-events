0be95377f8169e951cea918ccd222507dec92440
Hugo <hugo@macbook-pro-de-hugo.home>
2018-01-30 22:04:24 +0100

    a

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-$(document).ready(function() {
-      $(window).on('scroll', function() {
-        if (Math.round($(window).scrollTop()) > 30) {
-          $('.navbar').addClass('scrolled');
-        } else {
-          $('.navbar').removeClass('scrolled');
-        }
-      });
-
-});
-
 ;(function($) {
     "use strict";
     $(document).ready(function() {
